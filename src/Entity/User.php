a9d889f57b61cfd3453d23f8224daca417529af3
Mathieu Piot <math.piot@gmail.com>
2020-02-14 19:03:10 +0100

    Fix email case (Registration/login)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -71,7 +71,7 @@ class User implements UserInterface
 
     public function setEmail(string $email): self
     {
-        $this->email = $email;
+        $this->email = strtolower($email);
 
         return $this;
     }
