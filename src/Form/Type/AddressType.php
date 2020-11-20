26b20bb6dd21a7310b9f84171769f561d1a39211
Mathieu Piot <math.piot@gmail.com>
2020-11-20 19:43:36 +0100

    [Address] Lane number as string

--- src/Form/Type/AddressType.php
+++ src/Form/Type/AddressType.php
@@ -20,7 +20,6 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -30,7 +29,7 @@ class AddressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('laneNumber', NumberType::class, [
+            ->add('laneNumber', TextType::class, [
                 'label' => 'Numéro',
             ])
             ->add('laneType', ChoiceType::class, [
