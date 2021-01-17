76872756b5d00119766feb24fdc616315cb5add7
Mathieu Piot <math.piot@gmail.com>
2021-01-17 14:40:47 +0100

    [Physiology] Fix required form

--- src/Form/PhysiologyType.php
+++ src/Form/PhysiologyType.php
@@ -33,31 +33,25 @@ class PhysiologyType extends AbstractType
             ->add('lightAerobicHeartRateMin', IntegerType::class, [
                 'label' => 'B0 - UT2',
                 'help' => '65-70% FCmax',
-                'required' => false,
             ])
             ->add('heavyAerobicHeartRateMin', IntegerType::class, [
                 'label' => 'B1 - UT1',
                 'help' => '70-80% FCmax',
-                'required' => false,
             ])
             ->add('anaerobicThresholdHeartRateMin', IntegerType::class, [
                 'label' => 'B2 - AT',
                 'help' => '80-85% FCmax',
-                'required' => false,
             ])
             ->add('oxygenTransportationHeartRateMin', IntegerType::class, [
                 'label' => 'B3 - TR',
                 'help' => '85-95% FCmax',
-                'required' => false,
             ])
             ->add('anaerobicHeartRateMin', IntegerType::class, [
                 'label' => 'B5 - AN',
                 'help' => '95-100% FCmax',
-                'required' => false,
             ])
             ->add('maximumHeartRate', IntegerType::class, [
                 'label' => 'FCmax',
-                'required' => false,
             ])
             ->add('maximumOxygenConsumption', NumberType::class, [
                 'label' => 'VO2max',
