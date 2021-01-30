53f4fa6eee8403e07e8ca1e4f85b6d48fe2ed94a
Mathieu Piot <math.piot@gmail.com>
2021-01-30 12:58:54 +0100

    [1RM] Create page

---
+++ src/Controller/WorkoutMaximumLoadController.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\WorkoutMaximumLoad;
+use App\Form\WorkoutMaximumLoadType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/workout-maximum-load")
+ * @Security("(is_granted('ROLE_USER') and user.hasValidLicense()) or is_granted('ROLE_ADMIN')")
+ */
+class WorkoutMaximumLoadController extends AbstractController
+{
+    /**
+     * @Route("", name="workout_maximum_load_show", methods={"GET"})
+     */
+    public function show(): Response
+    {
+        return $this->render('workout_maximum_load/show.html.twig');
+    }
+
+    /**
+     * @Route("/edit", name="workout_maximum_load_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request): Response
+    {
+        $workoutMaximumLoad = $this->getUser()->getWorkoutMaximumLoad() ?? new WorkoutMaximumLoad($this->getUser());
+        $form = $this->createForm(WorkoutMaximumLoadType::class, $workoutMaximumLoad);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('workout_maximum_load_show');
+        }
+
+        return $this->render('workout_maximum_load/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ tests/Controller/WorkoutMaximumloadControllerTest.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Tests\Controller\Admin;
+
+use App\Factory\LicenseFactory;
+use App\Factory\WorkoutMaximumLoadFactory;
+use App\Tests\AppWebTestCase;
+
+class WorkoutMaximumloadControllerTest extends AppWebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testAccessDeniedForAnonymousUser($method, $url)
+    {
+        static::ensureKernelShutdown();
+        $client = static::createClient();
+        $client->request($method, $url);
+
+        $this->assertResponseRedirects('/login');
+    }
+
+    public function urlProvider()
+    {
+        yield ['GET', '/workout-maximum-load'];
+        yield ['GET', '/workout-maximum-load/edit'];
+    }
+
+    public function testShowWorkoutMaximumLoad()
+    {
+        $user = LicenseFactory::new()->annualActive()->withValidLicense()->create()->getUser();
+        WorkoutMaximumLoadFactory::createOne([
+            'user' => $user,
+        ]);
+
+        self::ensureKernelShutdown();
+        $client = static::createClient();
+        $client->loginUser($user);
+        $client->request('GET', '/workout-maximum-load');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testShowWorkoutMaximumLoadWithoutInfos()
+    {
+        $user = LicenseFactory::new()->annualActive()->withValidLicense()->create()->getUser();
+
+        self::ensureKernelShutdown();
+        $client = static::createClient();
+        $client->loginUser($user);
+        $client->request('GET', '/workout-maximum-load');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testEditWorkoutMaximumLoad()
+    {
+        $user = LicenseFactory::new()->annualActive()->withValidLicense()->create()->getUser();
+        $workoutMaximumLoad = WorkoutMaximumLoadFactory::createOne([
+            'user' => $user,
+        ]);
+
+        self::ensureKernelShutdown();
+        $client = static::createClient();
+        $client->loginUser($user);
+        $client->request('GET', '/workout-maximum-load/edit');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Sauver', [
+            'workout_maximum_load[rowingTirage]' => 1,
+            'workout_maximum_load[benchPress]' => 2,
+            'workout_maximum_load[squat]' => 3,
+            'workout_maximum_load[legPress]' => 4,
+            'workout_maximum_load[clean]' => 5,
+        ]);
+
+        $this->assertResponseRedirects();
+
+        $workoutMaximumLoad->refresh();
+
+        $this->assertSame(1, $workoutMaximumLoad->getRowingTirage());
+        $this->assertSame(2, $workoutMaximumLoad->getBenchPress());
+        $this->assertSame(3, $workoutMaximumLoad->getSquat());
+        $this->assertSame(4, $workoutMaximumLoad->getLegPress());
+        $this->assertSame(5, $workoutMaximumLoad->getClean());
+    }
+}
