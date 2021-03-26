cf9ffc7af024e1a59d7b0c56cf49756ca6b42824
Mathieu Piot <math.piot@gmail.com>
2021-03-27 00:19:47 +0100

    [User] Fix TypeHint (#101)

---
+++ migrations/Version20210326231213.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210326231213 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE app_user ALTER auth_code TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE app_user ALTER auth_code DROP DEFAULT');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE app_user ALTER auth_code TYPE INT');
+        $this->addSql('ALTER TABLE app_user ALTER auth_code DROP DEFAULT');
+    }
+}
