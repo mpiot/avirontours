17baf80f0a13b74bd51158b8bd3d03c19a1bcbfa
Mathieu Piot <math.piot@gmail.com>
2021-05-13 10:30:12 +0200

    Update friendsofphp/php-cs-fixer recipe

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
- * Copyright 2020 Mathieu Piot
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 $fileHeaderComment = <<<'COMMENT'
     Copyright 2020 Mathieu Piot
 
@@ -70,5 +54,5 @@ return (new PhpCsFixer\Config())
         'single_line_throw' => true,
     ])
     ->setFinder($finder)
-    ->setCacheFile(__DIR__.'/var/.php_cs.cache')
+    ->setCacheFile('.php-cs-fixer.cache') // forward compatibility with 3.x line
 ;
