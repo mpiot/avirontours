d728a56b66eeaaf28f0a31dc28f4b96598b3e53e
Mathieu Piot <math.piot@gmail.com>
2021-08-09 18:17:44 +0200

    Update navs

--- assets/controllers/app_menu_controller.js
+++ assets/controllers/app_menu_controller.js
@@ -2,17 +2,18 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     static targets = ['menu'];
+    static values = { autoOpen: Boolean }
 
     connect() {
         // On xl screen, display the menu automatically
-        if (window.innerWidth >= 1200) {
-            this.menuTarget.classList.add('nav-app-menu-show');
+        if (true === this.autoOpenValue && window.innerWidth >= 1200) {
+            this.element.classList.add('nav-app-menu-show');
         }
     }
 
     toggle(event) {
         event.stopPropagation();
-        this.menuTarget.classList.toggle('nav-app-menu-show');
+        this.element.classList.toggle('nav-app-menu-show');
     }
 
     main() {
@@ -20,10 +21,10 @@ export default class extends Controller {
             return;
         }
 
-        if (false === this.menuTarget.classList.contains('nav-app-menu-show')) {
+        if (false === this.element.classList.contains('nav-app-menu-show')) {
             return;
         }
 
-        this.menuTarget.classList.remove('nav-app-menu-show');
+        this.element.classList.remove('nav-app-menu-show');
     }
 }
