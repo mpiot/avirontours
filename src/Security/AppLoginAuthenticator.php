41cee99c984c604eb50eb1ce1cdbfa740bb0f8c7
Mathieu Piot <math.piot@gmail.com>
2021-08-10 15:33:00 +0200

    [Security] Use new Authenticator and lLogin Throttling (#134)
    * [Security] Update to Authenticator-based
    
    * Add login throttling
    
    * Fix dev 2fa
    
    * PHP-CS

--- src/Security/AppLoginAuthenticator.php
+++ src/Security/AppLoginAuthenticator.php
@@ -20,95 +20,61 @@ declare(strict_types=1);
 
 namespace App\Security;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
-use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use function Symfony\Component\String\u;
 
-class AppLoginAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
+class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
-    public function __construct(private EntityManagerInterface $entityManager, private UrlGeneratorInterface $urlGenerator, private CsrfTokenManagerInterface $csrfTokenManager, private UserPasswordEncoderInterface $passwordEncoder)
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
     {
     }
 
-    public function supports(Request $request)
+    public function authenticate(Request $request): PassportInterface
     {
-        return 'app_login' === $request->attributes->get('_route')
-            && $request->isMethod('POST');
-    }
+        $username = u($request->request->get('username'))->lower()->toString();
+        $password = $request->request->get('password');
+        $csrfToken = $request->request->get('_csrf_token');
 
-    public function getCredentials(Request $request)
-    {
-        $credentials = [
-            'username' => u($request->request->get('username'))->lower(),
-            'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
-        ];
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['username']
+        $request->getSession()->set(Security::LAST_USERNAME, $request->request->get('username'));
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($password),
+            [
+                new RememberMeBadge(),
+                new CsrfTokenBadge('authenticate', $csrfToken),
+            ]
         );
-
-        return $credentials;
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $firewallName): ?Response
     {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
-        if (!$this->csrfTokenManager->isTokenValid($token)) {
-            throw new InvalidCsrfTokenException();
-        }
-
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $credentials['username']]);
-
-        if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
-        }
-
-        return $user;
-    }
-
-    public function checkCredentials($credentials, UserInterface $user)
-    {
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
-    }
-
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function getPassword($credentials): ?string
-    {
-        return $credentials['password'];
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
 
         return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
-    protected function getLoginUrl()
+    protected function getLoginUrl(Request $request): string
     {
-        return $this->urlGenerator->generate('app_login');
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
     }
 }
---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,167 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Factory\UserFactory;
+use App\Tests\AppWebTestCase;
+
+class SecurityControllerTest extends AppWebTestCase
+{
+    public function testLogin(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Se connecter', [
+            'username' => 'firstname.lastname',
+            'password' => UserFactory::PASSWORD,
+        ]);
+
+        $this->assertResponseRedirects('/');
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLoginWithCaps(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Se connecter', [
+            'username' => 'FIRSTNAME.LASTNAME',
+            'password' => UserFactory::PASSWORD,
+        ]);
+
+        $this->assertResponseRedirects('/');
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLoginWithBadUsername(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Se connecter', [
+            'username' => 'IDoNotExist',
+            'password' => UserFactory::PASSWORD,
+        ]);
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertStringContainsString('Identifiants invalides.', $crawler->filter('div.alert-danger')->text());
+    }
+
+    public function testLoginWithBadPassword(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Se connecter', [
+            'username' => 'firstname.lastname',
+            'password' => 'IDoNotExist',
+        ]);
+
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertStringContainsString('Identifiants invalides.', $crawler->filter('div.alert-danger')->text());
+    }
+
+    public function testLoginWithBadUsernameAndIPTooManyTimes(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        for ($i = 0; $i < 5; ++$i) {
+            $client->submitForm('Se connecter', [
+                'username' => 'firstname.lastname',
+                'password' => 'IDoNotExist',
+            ]);
+
+            $this->assertResponseRedirects('/login');
+            $crawler = $client->followRedirect();
+            $this->assertStringContainsString('Identifiants invalides.', $crawler->filter('div.alert-danger')->text());
+        }
+
+        $client->submitForm('Se connecter', [
+            'username' => 'firstname.lastname',
+            'password' => UserFactory::PASSWORD,
+        ]);
+
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertStringContainsString('Plusieurs tentatives de connexion ont échoué, veuillez réessayer dans 1 minute.', $crawler->filter('div.alert-danger')->text());
+    }
+
+    public function testLoginWithIPTooManyTimes(): void
+    {
+        UserFactory::createOne(['firstName' => 'firstname', 'lastName' => 'lastname']);
+
+        static::ensureKernelShutdown();
+        $client = static::createClient([], ['REMOTE_ADDR' => UserFactory::faker()->ipv4()]);
+        $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+
+        for ($i = 0; $i < 25; ++$i) {
+            $client->submitForm('Se connecter', [
+                'username' => UserFactory::faker()->userName(),
+                'password' => 'IDoNotExist',
+            ]);
+
+            $this->assertResponseRedirects('/login');
+            $client->followRedirect();
+        }
+
+        $client->submitForm('Se connecter', [
+            'username' => 'firstname.lastname',
+            'password' => UserFactory::PASSWORD,
+        ]);
+
+        $this->assertResponseRedirects('/login');
+        $crawler = $client->followRedirect();
+        $this->assertStringContainsString('Plusieurs tentatives de connexion ont échoué, veuillez réessayer dans 1 minute.', $crawler->filter('div.alert-danger')->text());
+    }
+}
