9f19e34b59f13fb5c241597a7414c43b540c7efe
Mathieu Piot <math.piot@gmail.com>
2022-01-24 12:43:24 +0100

    Remove Discord DSN

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -5,7 +5,6 @@ return [
     'APP_IBAN' => null,
     'APP_LOGBOOK_USER_PASSWORD' => null,
     'DATABASE_URL' => null,
-    'DISCORD_DSN' => null,
     'MAILER_DSN' => null,
     'RECAPTCHA3_KEY' => null,
     'RECAPTCHA3_SECRET' => null,
