ac07c0610810a2c822648dc9843dfc04a13a12fa
Mathieu Piot <math.piot@gmail.com>
2022-01-24 11:54:22 +0100

    [ShellDamage] Fix notifications

---
+++ src/Notification/ShellDamageNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Notification;
+
+use App\Entity\ShellDamage;
+use App\Entity\ShellDamageCategory;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Recipient\RecipientInterface;
+
+class ShellDamageNotification extends Notification implements EmailNotificationInterface
+{
+    public function __construct(private ShellDamage $shellDamage)
+    {
+        $this->importance(ShellDamageCategory::PRIORITY_HIGH === $shellDamage->getCategory()->getPriority() ? Notification::IMPORTANCE_URGENT : Notification::IMPORTANCE_MEDIUM);
+
+        parent::__construct('Nouvelle avarie');
+    }
+
+    public function asEmailMessage(RecipientInterface $recipient, string $transport = null): ?EmailMessage
+    {
+        $message = EmailMessage::fromNotification($this, $recipient);
+        /** @var TemplatedEmail $rawMessage */
+        $rawMessage = $message->getMessage();
+        $rawMessage
+            ->htmlTemplate('emails/shell_damage_notification.html.twig')
+            ->context([
+                'shellDamage' => $this->shellDamage,
+                'footer_text' => null,
+            ])
+        ;
+
+        return $message;
+    }
+
+    public function getChannels(RecipientInterface $recipient): array
+    {
+        return ['email'];
+    }
+}
