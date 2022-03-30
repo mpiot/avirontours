e9a1ee03ad3cf13a9aa82a95b8f4ab4d9afe3e66
Mathieu Piot <math.piot@gmail.com>
2022-03-30 21:13:34 +0200

    Refactor charts

---
+++ src/Chart/PhysicalQualitiesChart.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Chart;
+
+use App\Entity\PhysicalQualities;
+use App\Entity\User;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+final class PhysicalQualitiesChart
+{
+    public function __construct(private ChartBuilderInterface $chartBuilder)
+    {
+    }
+
+    public function chart(User $user)
+    {
+        /** @var PhysicalQualities $physicalQualities */
+        $physicalQualities = $user->getPhysicalQualities();
+
+        if (null === $physicalQualities) {
+            return null;
+        }
+
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_RADAR);
+        $chart->setData([
+            'labels' => ['Proprioception', 'Poids/Puissance', 'Force explosive', 'Force d\'endurance', 'Force maximale', 'Résistance', 'Gainage', 'Souplesse', 'Récupération'],
+            'datasets' => [
+                [
+                    'label' => '',
+                    'backgroundColor' => 'rgba(54, 162, 235, 0.6)',
+                    'borderColor' => 'rgba(54, 162, 235, 1)',
+                    'data' => [
+                        $physicalQualities->getProprioception(),
+                        $physicalQualities->getWeightPowerRatio(),
+                        $physicalQualities->getExplosiveStrength(),
+                        $physicalQualities->getEnduranceStrength(),
+                        $physicalQualities->getMaximumStrength(),
+                        $physicalQualities->getStressResistance(),
+                        $physicalQualities->getCoreStrength(),
+                        $physicalQualities->getFlexibility(),
+                        $physicalQualities->getRecovery(),
+                    ],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'legend' => [
+                'display' => false,
+            ],
+            'scale' => [
+                'ticks' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 20,
+                ],
+            ],
+        ]);
+
+        return $chart;
+    }
+}
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -20,19 +20,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\PhysicalQualities;
-use App\Repository\LogbookEntryRepository;
+use App\Chart\LogbookChart;
+use App\Chart\PhysicalQualitiesChart;
 use App\Repository\SeasonCategoryRepository;
-use App\Service\ArrayNormalizer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
 
 class HomepageController extends AbstractController
 {
     #[Route(path: '', name: 'homepage')]
-    public function homepage(SeasonCategoryRepository $seasonCategoryRepository, LogbookEntryRepository $repository, ChartBuilderInterface $chartBuilder, ArrayNormalizer $normalizer): Response
+    public function homepage(SeasonCategoryRepository $seasonCategoryRepository, LogbookChart $logbookChart, PhysicalQualitiesChart $physicalQualitiesChart): Response
     {
         if (null === $this->getUser()) {
             return $this->render('homepage/homepage.html.twig', [
@@ -40,96 +37,9 @@ class HomepageController extends AbstractController
             ]);
         }
 
-        // Logbook chart
-        $logbookCount = $repository->findStatsByMonth($this->getUser());
-        $logbookCount = $normalizer->fillMissingMonths($logbookCount, (new \DateTime('-11 months')), (new \DateTime()), ['distance' => 0, 'session' => 0]);
-        $logbookCount = $normalizer->normalize($logbookCount);
-        $logbookCount = $normalizer->formatMonthNames($logbookCount);
-        $logbookChart = $chartBuilder->createChart(Chart::TYPE_BAR);
-        $logbookChart->setData([
-            'labels' => $logbookCount['months'],
-            'datasets' => [
-                [
-                    'label' => 'Distances',
-                    'yAxisID' => 'distances',
-                    'backgroundColor' => 'rgba(54, 162, 235, 0.6)',
-                    'borderColor' => 'rgba(54, 162, 235, 1)',
-                    'borderWidth' => 1,
-                    'data' => $logbookCount['distances'],
-                ],
-                [
-                    'label' => 'Sessions',
-                    'yAxisID' => 'sessions',
-                    'backgroundColor' => 'rgb(235,54,54, 0.6)',
-                    'borderColor' => 'rgb(235,54,54, 1)',
-                    'borderWidth' => 1,
-                    'data' => $logbookCount['sessions'],
-                ],
-            ],
-        ]);
-        $logbookChart->setOptions([
-            'scales' => [
-                'distances' => [
-                    'type' => 'linear',
-                    'position' => 'left',
-                    'ticks' => [
-                        'beginAtZero' => true,
-                        'precision' => 0,
-                    ],
-                ],
-                'sessions' => [
-                    'type' => 'linear',
-                    'position' => 'right',
-                    'ticks' => [
-                        'beginAtZero' => true,
-                        'precision' => 0,
-                    ],
-                ],
-            ],
-        ]);
-
-        // Physical qualities chart
-        /** @var PhysicalQualities $physicalQualities */
-        $physicalQualities = $this->getUser()->getPhysicalQualities();
-        if (null !== $physicalQualities) {
-            $physicalQualitiesChart = $chartBuilder->createChart(Chart::TYPE_RADAR);
-            $physicalQualitiesChart->setData([
-                'labels' => ['Proprioception', 'Poids/Puissance', 'Force explosive', 'Force d\'endurance', 'Force maximale', 'Résistance', 'Gainage', 'Souplesse', 'Récupération'],
-                'datasets' => [
-                    [
-                        'label' => '',
-                        'backgroundColor' => 'rgba(54, 162, 235, 0.6)',
-                        'borderColor' => 'rgba(54, 162, 235, 1)',
-                        'data' => [
-                            $physicalQualities->getProprioception(),
-                            $physicalQualities->getWeightPowerRatio(),
-                            $physicalQualities->getExplosiveStrength(),
-                            $physicalQualities->getEnduranceStrength(),
-                            $physicalQualities->getMaximumStrength(),
-                            $physicalQualities->getStressResistance(),
-                            $physicalQualities->getCoreStrength(),
-                            $physicalQualities->getFlexibility(),
-                            $physicalQualities->getRecovery(),
-                        ],
-                    ],
-                ],
-            ]);
-            $physicalQualitiesChart->setOptions([
-                'legend' => [
-                    'display' => false,
-                ],
-                'scale' => [
-                    'ticks' => [
-                        'suggestedMin' => 0,
-                        'suggestedMax' => 20,
-                    ],
-                ],
-            ]);
-        }
-
         return $this->render('homepage/dashboard.html.twig', [
-            'logbookChart' => $logbookChart,
-            'physicalQualitiesChart' => $physicalQualitiesChart ?? null,
+            'logbookChart' => $logbookChart->chart($this->getUser()),
+            'physicalQualitiesChart' => $physicalQualitiesChart->chart($this->getUser()),
         ]);
     }
 }
