f3f6acba9da7964e82e8fc92cd53304a07187766
Mathieu Piot <math.piot@gmail.com>
2023-09-01 17:26:44 +0200

    [Turnstile] Fix params (#292)

--- assets/controllers/turnstile_controller.js
+++ assets/controllers/turnstile_controller.js
@@ -3,22 +3,27 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ['container'];
     static values = {
-        siteKey: String
+        siteKey: String,
+        action: String,
+        theme: String
     };
 
     /**
      * @type {Object} turnstile
-     * @property {string} siteKeyValue
      * @property {HTMLFormElement} containerTarget
+     * @property {string} siteKeyValue
+     * property {string} actionValue
+     * property {string} themeValue
      */
 
     turnstileId;
 
     connect () {
-        console.log('we');
         turnstile.ready(() => {
             this.turnstileId = turnstile.render(this.containerTarget, {
                 sitekey: this.siteKeyValue,
+                action: this.actionValue,
+                theme: this.themeValue
             });
         });
     }
