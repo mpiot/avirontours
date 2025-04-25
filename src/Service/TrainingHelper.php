44b3dafe1db137eea3e3bb2baa2446dfb122e100
Mathieu Piot <math.piot@gmail.com>
2025-04-25 20:37:18 +0200

    [Training] Improve weekly summary (#376)

---
+++ src/Service/TrainingHelper.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright 2020 Mathieu Piot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace App\Service;
+
+use App\Entity\Training;
+use App\Entity\User;
+use App\Repository\TrainingRepository;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+readonly class TrainingHelper
+{
+    public function __construct(
+        private TrainingRepository $trainingRepository,
+        private TranslatorInterface $translator,
+    ) {
+    }
+
+    public function getTrainingsSummary(User $user, \DateTimeInterface $startAt, \DateTimeInterface $endAt): array
+    {
+        $weeks = new \DatePeriod(
+            $startAt,
+            new \DateInterval('P1W'),
+            $endAt,
+            \DatePeriod::INCLUDE_END_DATE
+        );
+
+        $data = [];
+        $trainings = $this->trainingRepository->findForUser($user, $startAt, $endAt);
+        foreach ($weeks as $week) {
+            $weekTrainings = array_filter(
+                $trainings,
+                fn (Training $training) => $week->format('W') === $training->getTrainedAt()->format('W'),
+            );
+
+            $categorizedTrainings = [];
+            foreach ($weekTrainings as $training) {
+                if (false === \array_key_exists($training->getSport()->value, $categorizedTrainings)) {
+                    $categorizedTrainings[$training->getSport()->value] = [
+                        'sport' => $training->getSport(),
+                        'sessions' => 0,
+                        'duration' => 0,
+                        'distance' => 0,
+                    ];
+                }
+
+                ++$categorizedTrainings[$training->getSport()->value]['sessions'];
+                $categorizedTrainings[$training->getSport()->value]['duration'] += (int) round($training->getDuration() / 10);
+                $categorizedTrainings[$training->getSport()->value]['distance'] += $training->getDistance();
+            }
+            usort(
+                $categorizedTrainings,
+                fn (array $a, array $b) => $this->translator->trans($a['sport']->label()) <=> $this->translator->trans($b['sport']->label())
+            );
+
+            $duration = array_reduce($weekTrainings, fn (int $carry, Training $training) => $carry + $training->getDuration(), 0);
+            $duration = (int) round($duration / 10);
+
+            $data[] = [
+                'week' => $week,
+                'trainings' => $weekTrainings,
+                'summary' => [
+                    'sessions' => \count($weekTrainings),
+                    'duration' => $duration,
+                    'sports' => $categorizedTrainings,
+                ],
+            ];
+        }
+
+        return $data;
+    }
+}
