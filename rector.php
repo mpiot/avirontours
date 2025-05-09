723af8a7a9b307a6864d74476da411618d0d4f57
Mathieu Piot <math.piot@gmail.com>
2025-05-09 21:48:09 +0200

    Rector dead code level 50

--- rector.php
+++ rector.php
@@ -30,7 +30,7 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     ->withTypeCoverageLevel(50)
-    ->withDeadCodeLevel(0)
+    ->withDeadCodeLevel(50)
     ->withCodeQualityLevel(0)
     ->withRules([
         InlineConstructorDefaultToPropertyRector::class,
