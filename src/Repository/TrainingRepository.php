aaaa3c8f8f6a73251e17a2a5c3c8226a1d9af84e
Mathieu Piot <math.piot@gmail.com>
2026-08-22 20:46:41 +0200

    [Training] Add filters

--- src/Repository/TrainingRepository.php
+++ src/Repository/TrainingRepository.php
@@ -22,10 +22,13 @@ namespace App\Repository;
 
 use App\Entity\Training;
 use App\Entity\User;
+use App\Enum\SportType;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
+
+use function Symfony\Component\String\u;
 
 /**
  * @method Training|null find($id, $lockMode = null, $lockVersion = null)
@@ -42,18 +45,42 @@ class TrainingRepository extends ServiceEntityRepository
         parent::__construct($registry, Training::class);
     }
 
-    public function findUserPaginated(User $user, int $page = 1): PaginationInterface
-    {
-        $query = $this->createQueryBuilder('training')
-            ->innerJoin('training.user', 'user')
-            ->where('user.id = :user')
+    public function findUserPaginated(
+        User $user,
+        int $page = 1,
+        ?\DateTimeInterface $from = null,
+        ?\DateTimeInterface $to = null,
+        ?SportType $sport = null,
+        ?string $query = null,
+    ): PaginationInterface {
+        $queryBuilder = $this->createQueryBuilder('training')
+            ->where('training.user = :user')
             ->orderBy('training.trainedAt', 'DESC')
-            ->setParameter('user', $user->getId())
-            ->getQuery()
+            ->setParameter('user', $user)
         ;
 
+        if (null !== $from) {
+            $queryBuilder->andWhere('training.trainedAt >= :from')->setParameter('from', $from);
+        }
+
+        if (null !== $to) {
+            $queryBuilder->andWhere('training.trainedAt <= :to')->setParameter('to', $to);
+        }
+
+        if (null !== $sport) {
+            $queryBuilder->andWhere('training.sport = :sport')->setParameter('sport', $sport);
+        }
+
+        $term = null !== $query ? u($query)->trim()->lower()->toString() : '';
+        if ('' !== $term) {
+            $queryBuilder
+                ->andWhere('LOWER(training.comment) LIKE :query')
+                ->setParameter('query', "%{$term}%")
+            ;
+        }
+
         return $this->paginator->paginate(
-            $query,
+            $queryBuilder->getQuery(),
             $page,
             Training::NUM_ITEMS
         );
